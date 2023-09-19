fe4fd1cd409ce5e18f0ab20701388b49a347ebc8
fc56366 <fc56366@alunos.fc.ul.pt>
2023-09-19 15:23:23 +0100

    Acabado serialization

--- source/serialization.c
+++ source/serialization.c
@@ -51,5 +51,36 @@ int keyArray_to_buffer(char** keys, char** keys_buf){
  * Retorna o array de strings ou NULL em caso de erro.
  */
 char** buffer_to_keyArray(char* keys_buf){
-    //TODO
+    // Verificar se o keys_buf é válido.
+    if(keys_buf == NULL) {
+        return NULL;
+    }
+
+    // Ler o nº de chaves (nkeys) do keys_buf.
+    int nkeys;
+    memcpy(&nkeys, keys_buf, sizeof(int)); //não se usa &keys_buf, pois keys_buf é um ponteiro!
+
+    // Mesmo processo/"método" do ponteiro para ler as keys no keys_buf.
+    char* keys_buf_ptr = keys_buf + sizeof(int);
+
+    // Alocar memória no array de strings.
+    char** keys = (char**)malloc((nkeys + 1) * sizeof(char*)); //+1 para o char nulo no final '\0'. e * sizeof(char*) por se tratarem de strings.
+    if (keys == NULL) {
+        return NULL; // Falha ao alocar memória nas keys!
+    }
+
+    // Copiar as chaves de keys_buf para o array keys.
+    for(int i = 0; i < nkeys; i++) {
+        keys[i] = strdup(keys_buf_ptr); // strdup aloca memória para a nova string/key e duplica.
+
+        if (keys[i] == NULL) { //caso haja erro a alocar memória para alguma key.
+            for(int j = 0; j < i; j++) {
+                free(keys[j]);
+            }
+            free(keys);
+            return NULL; //pois ocorreu erro a alocar memória a alguma key.
+        }
+        keys_buf_ptr += strlen(keys_buf_ptr) + 1; // Avança para a próxima key. Pois strlen devolve o tamanho da string + char nulo.
+    }
+
 }
