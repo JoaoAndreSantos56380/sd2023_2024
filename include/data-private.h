6bf7c431d146272c3054bf88f2dd66a0a746bf6d
joao_santos <joao.bf4@outlook.com>
2023-09-23 12:45:23 +0100

    data-private.h adicao de ifdef

---
+++ include/data-private.h
@@ -0,0 +1,9 @@
+#ifdef _DATA_PRIVATE_H
+#define _DATA_PRIVATE_H
+
+/* Função que valida um bloco de dados.
+ * Retorna 0 (OK) ou -1 em caso de erro.
+ */
+int data_valid(struct data_t* data);
+
+#endif
