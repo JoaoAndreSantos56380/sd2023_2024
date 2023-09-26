e8890880f003c79a71a98b19137babdc136bc61f
joao_santos <joao.bf4@outlook.com>
2023-09-26 16:23:45 +0100

    correcoes codigo para passar nos testes dos profs

--- source/table.c
+++ source/table.c
@@ -1,57 +1,51 @@
 #include "table.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "data-private.h"
 #include "list-private.h"
 #include "table-private.h"
 
-int module;	 // TODO posso ter esta variavel global?
-
 struct table_t* table_create(int n) {
-	// Verificar se o n não é negativo.
 	if (n <= 0) {
 		return NULL;
 	}
 
-	// Alocar memória à table e aos respetivos atributos.
-	struct table_t* table = malloc(sizeof(struct table_t));
+	struct table_t* table = (struct table_t*)malloc(sizeof(struct table_t));
 	if (table == NULL) {
 		return NULL;
 	}
-	table->lists = malloc(n * sizeof(struct list_t*));
+
+	table->size = 0;
+	table->rows = n;
+	table->lists = (struct list_t**)malloc(n * sizeof(struct list_t*));
 	if (table->lists == NULL) {
 		free(table);
 		return NULL;
 	}
-	table->size = 0;
 
-	// Criar as listas.
-	for (int i = 0; i < n; i++) {
+	// Inicializar as listas
+	for (int i = 0; i < table->rows; i++) {
 		table->lists[i] = list_create();
-
-		// Falha ao criar de uma lista, dar free da memória alocada às listas anteriores, às lists e à table.
 		if (table->lists[i] == NULL) {
 			for (int j = 0; j < i; j++) {
 				list_destroy(table->lists[j]);
 			}
 			free(table->lists);
 			free(table);
-
 			return NULL;
 		}
 	}
 
-	// module = n;
 	return table;
 }
 
 int table_destroy(struct table_t* table) {
-	// Verificar se a table é válida.
 	if (table == NULL) {
 		return -1;
 	}
 
-	// Destruir as listas da table e dar free do lists e da table.
-	for (size_t i = 0; i < table->size; i++) {
+	for (int i = 0; i < table->rows; i++) {
 		list_destroy(table->lists[i]);
 	}
 	free(table->lists);
@@ -61,20 +55,18 @@ int table_destroy(struct table_t* table) {
 }
 
 int table_put(struct table_t* table, char* key, struct data_t* value) {
-	// Verificar se os argumentos passados são válidos.
 	if (table == NULL) {
 		return -1;
 	}
+
 	if (key == NULL) {
 		return -1;
 	}
+
 	if (data_valid(value) == -1) {
 		return -1;
 	}
 
-	int hash = hash_code(key, table->size);
-
-	// Fazer cópias da key e do value.
 	char* key_copy = strdup(key);
 	if (key_copy == NULL) {
 		return -1;
@@ -87,18 +79,26 @@ int table_put(struct table_t* table, char* key, struct data_t* value) {
 	}
 
 	struct entry_t* new_entry = entry_create(key_copy, value_copy);
-	int result = list_add(table->lists[hash], new_entry);
+	if (new_entry == NULL) {
+		free(key_copy);
+		data_destroy(value_copy);
+		return -1;
+	}
 
-	// Libertar espaço alocado das cópias e da nova entry. Podiamos simplesmente fazer entry_destroy??
-	free(key_copy);
-	data_destroy(value_copy);
-	entry_destroy(new_entry);
+	int row = hash_code(key, table->rows);
+	struct list_t* list = table->lists[row];
+	int result = list_add(list, new_entry);
+	if (result == -1) {
+		entry_destroy(new_entry);
+		return -1;
+	} else if (result == 0) {
+		table->size++;
+	}
 
-	return (result >= 0) ? 0 : -1;
+	return 0;
 }
 
 struct data_t* table_get(struct table_t* table, char* key) {
-	// Verificar se os argumentos passados são válidos.
 	if (table == NULL) {
 		return NULL;
 	}
@@ -106,91 +106,105 @@ struct data_t* table_get(struct table_t* table, char* key) {
 		return NULL;
 	}
 
-	int hash = hash_code(key, table->size);
-	struct entry_t* entry = list_get(table->lists[hash], key);
-	if (entry == NULL) {	 // Se a key não for encontrada.
+	if (table->size == 0) {
+		return NULL;
+	}
+
+	int row = hash_code(key, table->rows);
+	struct entry_t* entry = list_get(table->lists[row], key);
+	if (entry == NULL) {
 		return NULL;
 	}
 	return data_dup(entry->value);
 }
 
 int table_remove(struct table_t* table, char* key) {
-	// Verificar se os argumentos são válidos.
 	if (table == NULL) {
-		return NULL;
+		return -1;
 	}
 	if (key == NULL) {
-		return NULL;
+		return -1;
 	}
 
-	int hash = hash_code(key, table->size);
-	return list_remove(table->lists[hash], key);
+	if (table->size == 0) {
+		return 1;
+	}
+
+	int row = hash_code(key, table->rows);
+	int result = list_remove(table->lists[row], key);
+	if (result == -1) {
+		return -1;
+	}
+	if (result == 0) {
+		table->size--;
+		return 0;
+	}
+	return 1;
 }
 
 int table_size(struct table_t* table) {
+	if (table == NULL) {
+		return -1;
+	}
 	return table->size;
 }
 
 char** table_get_keys(struct table_t* table) {
-	// Verificar se a table é válida.
 	if (table == NULL) {
 		return NULL;
 	}
 
-	char** keys = malloc((table_size(table) + 1) * sizeof(char*));
-	// Verificar se a memória foi alocada com sucesso.
+	if (table->size == 0) {
+		return NULL;
+	}
+
+	char** keys = (char**)calloc(table_size(table) + 1, sizeof(char*));
 	if (keys == NULL) {
 		return NULL;
 	}
 
-	char** current_key = keys;	 // Ponteiro para a posição atual do array de keys.
-	for (int i = 0; i < table->size; i++) {
-		struct list_t* current_list = table->lists[i];
+	int keyIndex = 0;
+	for (int row = 0; row < table->rows; row++) {
+		struct list_t* list = table->lists[row];
 
-		// Iterar sobre os nodes na lista.
-		struct node_t* current_node = current_list->head;
-		while (current_node != NULL) {
-			// Copiar a chave para o array de strings.
-			*current_key = strdup(current_node->entry->key);
-			if (*current_key == NULL) {
-				// Falha ao alocar memória para a key. Libertar toda a memória alocada até agora.
-				char** key_to_free = keys;	 // Novo ponteiro para ir dando free à memória das keys.
-				while (key_to_free < current_key) {
-					free(*key_to_free);
-					key_to_free++;
-				}
-				free(keys);
-				return NULL;
-			}
-
-			current_key++;
-			current_node = current_node->next;
+		char** listKeys = list_get_keys(list);
+		int listIndex = 0;
+		char* key = listKeys[listIndex];
+		while (key != NULL) {
+			char* key_copy = (char*)malloc(strlen(key) + 1);
+			strcpy(key_copy, key);
+			keys[keyIndex++] = key_copy;
+			key = listKeys[++listIndex];
 		}
+		list_free_keys(listKeys);
 	}
+
 	return keys;
 }
 
 int table_free_keys(char** keys) {
-	// Verificar se as keys são válidas.
+	//return list_free_keys(keys); POR ACASO o codigo é o mesmo por isso podemos chamar esta funcao
 	if (keys == NULL) {
 		return -1;
 	}
 
-	int rows = (sizeof(keys) / sizeof(*keys));
-	for (int i = 0; i < rows; i++) {
-		free(keys[i]);
+	int index = 0;
+	char* key = keys[index];
+	while (key != NULL) {
+		free(key);
+		key = keys[++index];
 	}
 	free(keys);
 
 	return 0;
 }
 
-int hash_code(char* key, int n) {
+int hash_code(char* key, int module) {
 	int index = 0;
 	int sum = 0;
 	while (key[index] != '\0') {
 		sum += key[index];
 		index++;
 	}
-	return sum % n;
+	return sum % module;
 }
