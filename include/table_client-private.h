31ae64bed115ef5823699b3861d33ba6fd1ceecd
joao_santos <joao.bf4@outlook.com>
2023-10-11 19:45:52 +0100

    adicao header table client

---
+++ include/table_client-private.h
@@ -0,0 +1,33 @@
+#ifndef _TABLE_CLIENT_PRIVATE_
+#define _TABLE_CLIENT_PRIVATE_
+void showMenu();
+
+void readOption(char* input, int size);
+
+int commandIsPut(char* option);
+
+int commandIsGet(char* option);
+
+int commandIsDel(char* option);
+
+int commandIsSize(char* option);
+
+int commandIsHeight(char* option);
+
+int commandIsGetKeys(char* option);
+
+void executePut(struct rtable_t* r_table, char* option);
+
+void executeGet(struct rtable_t* r_table, char* option);
+
+void executeDel(struct rtable_t* r_table, char* option);
+
+void executeSize(struct rtable_t* r_table);
+
+void executeHeight(struct rtable_t* r_table);
+
+void executeGetTable(struct rtable_t* r_table);
+
+void executeGetKeys(struct rtable_t* rtable);
+
+#endif
