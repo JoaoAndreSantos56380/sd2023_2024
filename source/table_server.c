49870eca82a5c4e254173a9cccb0b2b99cf1cb42
fc56366 <fc56366@alunos.fc.ul.pt>
2023-10-11 15:56:09 +0100

    Acabado table_server

--- source/table_server.c
+++ source/table_server.c
@@ -1,1 +1,39 @@
-/*Fazer o que é pedido no enunciado; Mas talvez fazer os outros .c primeiro*/
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <arpa/inet.h>
+
+#include "network_server.h"
+#include "table_skel.h"
+
+int main(int argc, char **argv) {
+    int port, listening_socket, n_lists;
+
+    // verifica os argumentos passados
+    if (argc != 3) {
+        printf("Uso: ./table-server <port> <n_lists>\n");
+        printf("Exemplo de uso: ./table-server 12345 3\n");
+        return -1;
+    }
+
+    port = atoi(argv[1]);
+    if((listening_socket = network_server_init(port)) < 0) {
+        return -1;
+    }
+
+    n_lists = atoi(argv[2]);
+    if(n_lists <= 0) {
+        return -1;
+    }
+
+    struct table_t *table = table_skel_init(n_lists);
+
+    int result = network_main_loop(listening_socket, table);
+
+    if(result == -1) {
+        perror("Erro ao desconectar do servidor\n");
+    }
+
+    table_skel_destroy(table);
+    return result;
+}
