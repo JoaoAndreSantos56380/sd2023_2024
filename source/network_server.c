264a938a340af61313fd7077050ff1d317bfb0be
joao_santos <joao.bf4@outlook.com>
2023-10-20 12:41:15 +0100

    adicao do network server

--- source/network_server.c
+++ source/network_server.c
@@ -1,56 +1,120 @@
-#ifndef _NETWORK_SERVER_H
-#define _NETWORK_SERVER_H
+#include <errno.h>
+#include <netinet/in.h>
+#include <netinet/ip.h> /* superset of previous */
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <table.h>
+#include <unistd.h>
 
-#include "table.h"
-#include "sdmessage.pb-c.h"
-
-/* Função para preparar um socket de receção de pedidos de ligação
+#include "message-private.h"
+#include "network_server.h"
+#include "table_skel.h"
+int listening_socket;
+extern struct table_t* table;
+/* Função para preparar uma socket de receção de pedidos de ligação
  * num determinado porto.
- * Retorna o descritor do socket ou -1 em caso de erro.
+ * Retornar descritor do socket (OK) ou -1 (erro).
  */
 int network_server_init(short port) {
-    
+	// socket info struct
+	struct sockaddr_in server_info = {0};
+	server_info.sin_family = AF_INET;
+	server_info.sin_port = htons(port);
+	socklen_t server_info_len = sizeof(struct sockaddr_in);
+
+	// Ignore SIGPIPE signal so server doesn't crash if socket closes unexpectedly
+	struct sigaction new_actn;
+	new_actn.sa_handler = SIG_IGN;
+	sigemptyset(&new_actn.sa_mask);
+	new_actn.sa_flags = 0;
+	sigaction(SIGPIPE, &new_actn, NULL);
+
+	// socket
+	//  +++int listening_socket = socket(AF_INET, SOCK_STREAM, 0);
+	if ((listening_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		perror("socket");
+		return -1;
+	}
+
+	int option_value = 1;
+	setsockopt(listening_socket, SOL_SOCKET, SO_REUSEADDR, &option_value, sizeof(int));
+	// bind to addr
+	if (bind(listening_socket, (struct sockaddr*)&server_info, server_info_len) < 0) {
+		perror("bind");
+		return -1;
+	}
+
+	// listen for connections
+	if (listen(listening_socket, 0) < 0) {	 // right declaration
+		perror("listen failed");
+		return -1;
+	}
+	printf("Listening on port %hu...\n", port);
+
+	return listening_socket;
 }
 
-/* A função network_main_loop() deve:
+/* Esta função deve:
  * - Aceitar uma conexão de um cliente;
  * - Receber uma mensagem usando a função network_receive;
- * - Entregar a mensagem de-serializada ao skeleton para ser processada
-     na tabela table;
+ * - Entregar a mensagem de-serializada ao skeleton para ser processada;
  * - Esperar a resposta do skeleton;
  * - Enviar a resposta ao cliente usando a função network_send.
- * A função não deve retornar, a menos que ocorra algum erro. Nesse
- * caso retorna -1.
  */
-int network_main_loop(int listening_socket, struct table_t *table) {
+int network_main_loop(int listening_socket, struct table_t* table) {
+	struct sockaddr client_info = {0};
+	socklen_t client_info_len = sizeof(client_info);
+	int client_socket;
+	while ((client_socket = accept(listening_socket, &client_info, &client_info_len)) > 0) {
+		printf("Client connected\n");
 
+		struct message_t* msg;
+		while ((msg = network_receive(client_socket)) != NULL) {
+			invoke(msg, table);
+			network_send(client_socket, msg);
+		}
+		close(client_socket);
+		printf("Client disconnected\n");
+	}
+	return 0;
 }
 
-/* A função network_receive() deve:
+/* Esta função deve:
  * - Ler os bytes da rede, a partir do client_socket indicado;
  * - De-serializar estes bytes e construir a mensagem com o pedido,
- *   reservando a memória necessária para a estrutura MessageT.
- * Retorna a mensagem com o pedido ou NULL em caso de erro.
+ *   reservando a memória necessária para a estrutura message_t.
  */
-MessageT *network_receive(int client_socket) {
-
+struct message_t* network_receive(int client_socket) {
+	char* buff = (char*)malloc(1024);
+	int size = read_all(client_socket, &buff, 1024);
+	struct message_t* result = size > 0 ? message_t__unpack(NULL, size, (uint8_t*)buff) : NULL;
+	free(buff);
+	return result;
 }
 
-/* A função network_send() deve:
+/* Esta função deve:
  * - Serializar a mensagem de resposta contida em msg;
+ * - Libertar a memória ocupada por esta mensagem;
  * - Enviar a mensagem serializada, através do client_socket.
- * Retorna 0 (OK) ou -1 em caso de erro.
  */
-int network_send(int client_socket, MessageT *msg) {
-
+int network_send(int client_socket, struct message_t* msg) {
+	char* buffer = (char*)malloc(1024);
+	int buffer_size = message_t__pack(msg, (uint8_t*)buffer);
+	message_t__free_unpacked(msg, NULL);
+	int num_bytes_written = write_all(client_socket, buffer, buffer_size);
+	free(buffer);
+	return num_bytes_written;
 }
 
-/* Liberta os recursos alocados por network_server_init(), nomeadamente
- * fechando o socket passado como argumento.
- * Retorna 0 (OK) ou -1 em caso de erro.
+/* A função network_server_close() liberta os recursos alocados por
+ * network_server_init().
  */
 int network_server_close(int socket) {
-
+	//close(listening_socket);  // we might not need this one anymore, for further testing
+	close(socket);	 // we might not need this one anymore, for further testing
+	return 0;
 }
-
-#endif
